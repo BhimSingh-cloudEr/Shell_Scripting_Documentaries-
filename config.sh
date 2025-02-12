868e371cc5c406f6d73fa2e6f3e18fdaf1a6057f
Bhim Singh <160112851+BhimSingh-cloudEr@users.noreply.github.com>
2025-02-12 14:26:05 +0530

    Update Repo

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configuration Setting Commands
+
+DB_HOST="localhost"
+DB_PORT="3306"
+DB_USER="myuser"
+DB_PASSWORD="mypassword"
+
---
+++ main.sh
@@ -0,0 +1,14 @@
+# Source of the configuration file
+
+source config.sh
+
+# Now you can use the variable from the configuration file
+
+echo "Database host : ${DB_HOST}"
+
+echo "Database host : ${DB_PORT}"
+
+echo "Database host : ${DB_USER}"
+
+echo "Database host : ${DB_PASSWORD}"
+
